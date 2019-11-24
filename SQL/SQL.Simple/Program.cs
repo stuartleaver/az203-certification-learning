c6fabf599d1a6965ae030b49d99b2dfb06c48b0e
stuartleaver <stuartleaveruk@gmail.com>
2019-11-24 17:08:10 +0000

    Create Azure SQL Database example
    Created a really simple app that will output a list of names selected from a table in the AdventureWorks database.

---
+++ SQL/SQL.Simple/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace SQL.Simple
+{
+    class Program
+    {
+        public static IConfigurationRoot _configuration;
+
+        static async Task Main(string[] args)
+        {
+            Startup();
+
+            var sql = "SELECT FirstName, LastName FROM SalesLT.Customer";
+
+            using(var conn = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                using(var command = new SqlCommand(sql, conn))
+                {
+                    conn.Open();
+
+                    var reader = await command.ExecuteReaderAsync();
+
+                    while(reader.Read())
+                    {
+                        Console.WriteLine($"{reader["FirstName"]} {reader["LastName"]}");
+                    }
+
+                    conn.Close();
+                }
+            }
+        }
+
+        public static void Startup()
+        {
+           var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            
+            _configuration = builder.Build();
+        }
+    }
+}
